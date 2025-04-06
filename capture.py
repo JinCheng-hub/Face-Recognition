23ee5dfc29a875cddfbc49ca1b3e1eaaebae7713
Jin Cheng <a939709950@gmail.com>
2025-04-06 23:10:17 +0800

    Refactor: use argparse for input parameters

--- capture.py
+++ capture.py
@@ -1,39 +1,55 @@
 import os
 import cv2
+import argparse
 
-name = input("Name: ")
-detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-cap = cv2.VideoCapture(0)
 
-if not cap.isOpened():
-    exit()
+def capture(name, limit):
+    detector = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
+    cap = cv2.VideoCapture(0)
 
-root = os.path.join("dataset", name)
-os.makedirs(root, exist_ok=True)
-count = 0
+    if not cap.isOpened():
+        exit()
 
-while True:
-    ret, frame = cap.read()
+    root = os.path.join("dataset", name)
+    os.makedirs(root, exist_ok=True)
+    count = 0
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = detector.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
+    while True:
+        ret, frame = cap.read()
 
-    for x, y, w, h in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = detector.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
 
-    cv2.imshow("Face Detection", frame)
+        for x, y, w, h in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-    key = cv2.waitKey(5)
-    if key == ord("c"):
-        count += 1
-        face = gray[y : y + h, x : x + w]
-        resized_face = cv2.resize(face, (100, 100))
-        cv2.imwrite(f"{root}/{count}.jpg", resized_face)
-        print(f"Saved {count}.jpg")
+        cv2.imshow("Face Detection", frame)
 
-    elif key == ord("q"):
-        print(f"Saved {count} faces")
-        break
+        key = cv2.waitKey(5)
+        if key == ord("c"):
+            count += 1
+            face = gray[y : y + h, x : x + w]
+            resized_face = cv2.resize(face, (100, 100))
+            cv2.imwrite(f"{root}/{count}.jpg", resized_face)
+            print(f"Saved {count}.jpg")
 
-cap.release()
-cv2.destroyAllWindows()
+        elif (limit and count >= limit) or key == ord("q"):
+            print(f"Saved {count} faces")
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--name", help="誰的臉", required=True)
+    parser.add_argument("--limit", type=int, help="擷取上限", default=None)
+    args = parser.parse_args()
+    capture(args.name, args.limit)
+
+
+if __name__ == "__main__":
+    main()
--- train.py
+++ train.py
@@ -1,25 +1,39 @@
 import os
 import cv2
 import numpy as np
+import argparse
 
-data = input("Data path: ")
 
-detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')  # 載入人臉追蹤模型
-recog = cv2.face.LBPHFaceRecognizer_create()      # 啟用訓練人臉模型方法
-faces = []   # 儲存人臉位置大小的串列
-ids = []     # 記錄該人臉 id 的串列
+def train(data):
+    detector = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )  # 載入人臉追蹤模型
+    recog = cv2.face.LBPHFaceRecognizer_create()  # 啟用訓練人臉模型方法
+    faces = []  # 儲存人臉位置大小的串列
+    ids = []  # 記錄該人臉 id 的串列
 
-for i, name in enumerate(os.listdir(data)):
-    for file in os.listdir(f"{data}/{name}"):
-        img = cv2.imread(f"{data}/{name}/{file}")
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img_np = np.array(gray, "uint8")
-        face = detector.detectMultiScale(gray)
-        for (x, y, w, h) in face:
-            faces.append(img_np[y:y+h, x:x+w])
-            ids.append(i)
+    for i, name in enumerate(os.listdir(data)):
+        for file in os.listdir(f"{data}/{name}"):
+            img = cv2.imread(f"{data}/{name}/{file}")
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img_np = np.array(gray, "uint8")
+            face = detector.detectMultiScale(gray)
+            for x, y, w, h in face:
+                faces.append(img_np[y : y + h, x : x + w])
+                ids.append(i)
 
-print('training...')                              # 提示開始訓練
-recog.train(faces,np.array(ids))                  # 開始訓練
-recog.save('face.yml')                            # 訓練完成儲存為 face.yml
-print('ok!')
+    print("training...")  # 提示開始訓練
+    recog.train(faces, np.array(ids))  # 開始訓練
+    recog.save("face.yml")  # 訓練完成儲存為 face.yml
+    print("ok!")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data", help="訓練資料", required=True)
+    args = parser.parse_args()
+    train(args.name, args.limit)
+
+
+if __name__ == "__main__":
+    main()
