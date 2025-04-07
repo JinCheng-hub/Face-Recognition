9fa6f2286bfdcfc017cd3895650192b70fb44ac1
Jin Cheng <a939709950@gmail.com>
2025-04-07 12:12:39 +0800

    Refactor: improve face detection logic

--- capture.py
+++ capture.py
@@ -4,9 +4,10 @@ import argparse
 
 
 def capture(name, limit):
-    detector = cv2.CascadeClassifier(
+    face_detector = cv2.CascadeClassifier(
         cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
     )
+    eye_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
     cap = cv2.VideoCapture(0)
 
     if not cap.isOpened():
@@ -20,22 +21,25 @@ def capture(name, limit):
         ret, frame = cap.read()
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = detector.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
+        faces = face_detector.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
 
         for x, y, w, h in faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            face = gray[y : y + h, x : x + w]
+            eyes = face_detector.detectMultiScale(face, scaleFactor=1.05, minNeighbors=5)
+            if len(eyes) == 2:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                count += 1
+                resized_face = cv2.resize(face, (100, 100))
+                cv2.imwrite(f"{root}/{count}.jpg", resized_face)
+                print(f"Saved {count}.jpg")
 
         cv2.imshow("Face Detection", frame)
 
-        key = cv2.waitKey(5)
-        if key == ord("c"):
-            count += 1
-            face = gray[y : y + h, x : x + w]
-            resized_face = cv2.resize(face, (100, 100))
-            cv2.imwrite(f"{root}/{count}.jpg", resized_face)
-            print(f"Saved {count}.jpg")
+        if cv2.waitKey(3) & 0xFF == ord('q'):
+            print(f"Exiting...\n\nSaved {count} faces")
+            break
 
-        elif (limit and count >= limit) or key == ord("q"):
+        if count >= limit:
             print(f"Saved {count} faces")
             break
 
@@ -46,7 +50,7 @@ def capture(name, limit):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--name", help="誰的臉", required=True)
-    parser.add_argument("--limit", type=int, help="擷取上限", default=None)
+    parser.add_argument("--limit", type=int, help="擷取上限", default=25)
     args = parser.parse_args()
     capture(args.name, args.limit)
 
